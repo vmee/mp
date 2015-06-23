84731a60e23c382d83909e845b588b1f2877160f
vmee <konsj@sina.cn>
2015-06-23 11:48:17 +0800

    all

---
+++ conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_NAME' => 'weixin',
+  'DB_USER' => 'root',
+  'DB_PWD' => '52jscn',
+  'DB_PREFIX' => 'tp_',
+);
+
+?>
---
+++ conf/info.php
@@ -0,0 +1,16 @@
+<?php 
+return array (
+  'site_name' => '锦尚中国仿微盟微信公众平台最新版',
+  'site_title' => '锦尚中国仿微盟微信公众平台最新版',
+  'site_url' => 'http://demo.52jscn.com',
+  'site_my' => '锦尚中国仿微盟微信公众平台最新版',
+  'ischeckuser' => 'false',
+  'ipc' => '',
+  'site_qq' => '278869155',
+  'baidu_map_api' => 'QXpQ9NaEOoovpVhBcL9pM4Wi',
+  'site_email' => '278869155@qq.com',
+  'keyword' => '锦尚中国仿微盟微信公众平台最新版',
+  'content' => '锦尚中国仿微盟微信公众平台最新版',
+  'counts' => '',
+  'copyright' => '                        Copyright © 2012-2014 锦尚中国 版权所有',
+);
